0a26da896100ae91c93ced0c7d29adfe12feacda
syahanwar <syahanwarbk@gmail.com>
2024-04-11 15:54:14 +0800

    added route table

---
+++ aws_route_table.tf
@@ -0,0 +1,17 @@
+resource "aws_route_table" "prod-round-table" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod-RT"
+  }
+}
